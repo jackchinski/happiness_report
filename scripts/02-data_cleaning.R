fb8f446f3886549d8eab73dd5728999aa88c46d4
jackchinski <marcin.jacz@gmail.com>
2023-03-16 02:16:18 -0400

    master clean data frame with all fields and cleaned entries

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -49,13 +49,53 @@ clean5$new_income_range <- ifelse(clean5$income16 %in% c("UNDER $1 000", "$1 000
                                             ifelse(clean5$income16 %in% c("$40000 TO 49999", "$50000 TO 59999", "$60000 TO 74999", "$75000 TO $89999"), "40 000 to 89 999", 
                                                    ifelse(clean5$income16 %in% c("$90000 TO $109999", "$110000 TO $129999", "$130000 TO $149999", "$150000 TO $169999", "$170000 OR OVER"), "90 000 to 170 000", "other")))))
 
-#ifelse(clean3$income16 %in% c("$10000 TO 12499", "31000 to 40000"), "21000 to 40000", "other"))
-
-#hex <- table(clean3)
 
 
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+
+
+master_clean <- GSS %>% select(happy, hapmar, health, finalter, satfin, income16)
+
+master_clean1 <- subset(master_clean, income16 != ".d:  Do not Know/Cannot Choose")
+rm(master_clean)
+
+master_clean2 <- subset(master_clean1, income16 != ".r:  Refused")
+rm(master_clean1)
+
+master_clean3 <- subset(master_clean2, income16 != ".s:  Skipped on Web")
+rm(master_clean2)
+
+master_clean4 <- subset(master_clean3, satfin != ".n:  No answer")
+rm(master_clean3)
+
+master_clean5 <- subset(master_clean4, satfin != ".s:  Skipped on Web")
+rm(master_clean4)
+
+master_clean5$new_income_range <- ifelse(master_clean5$income16 %in% c("UNDER $1 000", "$1 000 TO 2 999", "$3 000 TO 3 999", "$4 000 TO 4 999", "$5 000 TO 5 999", "$6 000 TO 6 999",
+                                                         "$7 000 TO 7 999", "$8 000 TO 9 999"), "0 to 9 999",
+                                  ifelse(master_clean5$income16 %in% c("$10000 TO 12499", "$12500 TO 14999", "$15000 TO 17499", "$17500 TO 19999"), "10 000 to 19 999",
+                                         ifelse(master_clean5$income16 %in% c("$20000 TO 22499", "$22500 TO 24999", "$25000 TO 29999", "$30000 TO 34999", "$35000 TO 39999"), "20 000 to 39 999", 
+                                                ifelse(master_clean5$income16 %in% c("$40000 TO 49999", "$50000 TO 59999", "$60000 TO 74999", "$75000 TO $89999"), "40 000 to 89 999", 
+                                                       ifelse(master_clean5$income16 %in% c("$90000 TO $109999", "$110000 TO $129999", "$130000 TO $149999", "$150000 TO $169999", "$170000 OR OVER"), "90 000 to 170 000", "other")))))
+
+master_clean6 <- subset(master_clean5, hapmar != ".i:  Inapplicable")
+rm(master_clean5)
+
+master_clean7 <- subset(master_clean6, finalter != ".s:  Skipped on Web")
+rm(master_clean6)
+
+master_clean8 <- subset(master_clean7, health != ".s:  Skipped on Web")
+rm(master_clean7)
+
+master_clean9 <- subset(master_clean8, hapmar != ".n:  No answer")
+rm(master_clean8)
+
+master_clean10 <- subset(master_clean9, hapmar != ".s:  Skipped on Web")
+rm(master_clean9)
+
+master_clean11 <- subset(master_clean10, happy != ".s:  Skipped on Web")
+rm(master_clean10)
+
+
+
+
